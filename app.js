c3cbc6bd3142fbe4b5608e2995727809936cb050
Ramesh Bankuru <admin@Admins-MacBook-Pro.local>
2024-09-14 20:11:59 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+// React Element component
+const title = "Hashitha"
 
-
-const heading = React.createElement("div", { className: "parent" },
-    React.createElement("div", { className: "child" },
-        [
-            React.createElement("h1", { className: "heading" }, "Ramesh"),
-            React.createElement("h2", { className: "heading" }, "Bankuru ")
-        ]
-    ));
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(heading);
-root.render(heading);
+// function component
+const Heading = () => (
+    <div class="parent" > Ramesh Bankuru</div>
+)
+// function component
+const Head = () => (
+    <div>
+        {title}
+        <Heading />
+        <div class="parent" > Functional component</div>
+    </div>
+)
+// const heading = React.createElement("div", { className: "parent" }, "Ramesh");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Head />);
+//root.render(heading)
