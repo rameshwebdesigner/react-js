75c83ec4fbc402d36f4a58e809f2e02baf54df54
Ramesh Bankuru <admin@Admins-MacBook-Pro.local>
2024-09-19 20:41:42 +0530

    episode-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,93 +1,24 @@
 import { ResturantCard } from "./ResturantCard";
-// import { resList } from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Body = () => {
-    let restaurentList = [
-        {
-            "info": {
-                "id": "509004",
-                "name": "Chinese Wok",
-                "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
-                "locality": "Serilingampally Mandal",
-                "areaName": "Madhapur",
-                "costForTwo": "₹250 for two",
-                "cuisines": [
-                    "Chinese",
-                    "Asian",
-                    "Tibetan",
-                    "Desserts"
-                ],
-                "avgRating": 4.1,
-                "parentId": "61955",
-                "avgRatingString": "4.1",
-                "totalRatingsString": "2.1K+",
-                "sla": {
-                    "deliveryTime": 29,
-                    "lastMileTravel": 2.7,
-                    "serviceability": "SERVICEABLE",
-                    "slaString": "25-30 mins",
-                    "lastMileTravelString": "2.7 km",
-                    "iconType": "ICON_TYPE_EMPTY"
-                },
 
-            },
-        },
-        {
-            "info": {
-                "id": "337235",
-                "name": "Sai Darshini Tiffins",
-                "cloudinaryImageId": "mlhkup9htisw4xweuqsp",
-                "locality": "Ameerpet",
-                "areaName": "Sanath Nagar",
-                "costForTwo": "₹120 for two",
-                "cuisines": [
-                    "South Indian"
-                ],
-                "avgRating": 4.5,
-                "parentId": "11825",
-                "avgRatingString": "4.5",
-                "totalRatingsString": "13K+",
-                "sla": {
-                    "deliveryTime": 30,
-                    "lastMileTravel": 4.1,
-                    "serviceability": "SERVICEABLE",
-                    "slaString": "30-35 mins",
-                    "lastMileTravelString": "4.1 km",
-                    "iconType": "ICON_TYPE_EMPTY"
-                }
+    const [restsList, setRestsList] = useState([]);
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.454467&lng=78.4034188&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setRestsList(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
 
-            }
-        },
-        {
-            "info": {
-                "id": "785248",
-                "name": "Swach -South Indian Cafe",
-                "cloudinaryImageId": "cc66fd9193e9b8e44aaa74f77200e28b",
-                "locality": "Madhapur",
-                "areaName": "Madhapur",
-                "costForTwo": "₹140 for two",
-                "cuisines": [
-                    "South Indian"
-                ],
-                "avgRating": 4.6,
-                "parentId": "467456",
-                "avgRatingString": "4.6",
-                "totalRatingsString": "2.8K+",
-                "sla": {
-                    "deliveryTime": 29,
-                    "lastMileTravel": 2.5,
-                    "serviceability": "SERVICEABLE",
-                    "slaString": "25-30 mins",
-                    "lastMileTravelString": "2.5 km",
-                    "iconType": "ICON_TYPE_EMPTY"
-                },
+    useEffect(() => {
+        fetchData();
+        console.log("called useEffect");
+    }, []);
 
-            }
-        },
-    ]
-    const [restsList, setRestsList] = useState(restaurentList);
-
+    if (restsList.length === 0) {
+        return ("Loading...")
+    }
 
     return (
         <div className="body">
--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -5,14 +5,14 @@ const styleCard = {
 };
 export const ResturantCard = (props) => {
     const { resData } = props;
-    const { name, areaName, cloudinaryImageId, avgRating } = resData?.info;
+    const { name, areaName, cloudinaryImageId, avgRatingString } = resData?.info;
 
     return (
         <div className="res-card">
             <img className="res-img" src={REST_IMG + cloudinaryImageId} />
             <h4>{name}</h4>
             <h4>{areaName}</h4>
-            <h4>{avgRating}</h4>
+            <h4>{avgRatingString}</h4>
         </div>
     );
 };
